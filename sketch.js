6b587f0adce0fb2141b78fc72063ed7a8f900898
handmanartmatlala <92107274+handmanartmatlala@users.noreply.github.com>
2021-10-28 19:45:28 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,74 +1,57 @@
-let grass; 
-let bug;
-let snail;
-let grasshopper;
-let buzz;
-let circleX = 100; 
-let house;
-let decoration; 
+let pumpkin; 
+var faces = [];  
+var index = 0
+let knife;
+let wood; 
+let tree; 
+let s = '"Click to carve the pumpkin"';
 
-function preload(){
-  song = loadSound('stranger-than-fiction-8383.mp3')
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  grass = loadImage("grass-6110426_960_720.png"); 
-  bug = loadImage("bug.gif");
-  snail = loadImage("snail.gif");
-  grasshopper = loadImage("buggy.gif"); 
-  buzz = loadImage("buzz whoa.gif"); 
-  house = loadImage("giphy (5).gif");
-  decoration = loadImage("giphy (16).gif"); 
-  song.loop();
-  
-}
-
-function mousePressed() {
-  if (song.isPlaying()) {
-    // .isPlaying() returns a boolean
-    song.pause(); // .play() will resume from .pause() position
-    background(255, 0, 0);
-  } else {
-    song.play();
-    background(0, 255, 0);
-  }
+  pumpkin = loadImage("pump.png"); 
+  wood = loadImage("wood-2045379_960_720.jpg");
+  tree = loadImage("tree-2773505_960_720.png");
+ // faces = loadImage("face1.png","happy face.gif", "face2.png","suspscious.gif", "face3.png"); 
+  faces[0] = loadImage("face1.png");
+  faces[1] = loadImage("face2.png");
+  faces[2] = loadImage("face3.png");
+  faces[3] = loadImage("happy face.gif"); 
+  faces[4] = loadImage("suspscious.gif"); 
+  idx = 0;
+  knife = loadImage("knife.png"); 
+ 
 }
 
 function draw() {
-  background(232, 140, 49);
-  noStroke()
+  background(224, 196, 146);
   
-  //house1
-  image(house,600,-150,650,650)
-  //grass
-  image(grass, 0,-650,windowWidth+800, windowHeight+800)
+  //pumpkin face
+  image(tree,650,-1000,windowWidth+1000, windowHeight+1000)
+
+  image(wood,0, 800,windowWidth+800,windowHeight+800);
+  image(pumpkin,0,0); 
   
-  //ground
+  image(faces[idx],1350,950, -800 , -800 );
   
-  fill(117, 95, 34)
-  rect(0,500,windowWidth+800, windowHeight+200)
+  textSize(70);
+  fill(0);
+  textFont('Georgia');
+  text(s, 50, 50, 400, 700); 
+
+//  image(faces[index], 20, 200);
   
-  //house2
-  image(house,-100,-350,950,950)
+  image(knife, mouseX, mouseY);
   
-  //decorations
-   for (var x = 0; x < width; x = x + random(500, 500)) {
-    image(decoration, -50 + x, 0, 500, 500);
-  }
-  
- //ant
-  image(bug,200,400,250, 250)
-  
-  //grasshopper
-  
-  image(grasshopper, 800, 0, 700,700);
-  
-  //buzz
-  image(buzz,mouseX, mouseY)
-  
-  //grassfront
-  image(grass, 0,-300,windowWidth+800, windowHeight+800)
-  
- 
+
 }
+  
+function mouseClicked() {
+  idx++;
+  if (idx > faces.length - 1) {
+    idx = 0;
+  }
+}
+ // function mousePressed() {
+   // index = Math.floor(random(0, faces.length));
+//}
